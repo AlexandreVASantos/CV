4ae485db975dbd0f461b638e2bd5f0642b5f0418
AlexandreVASantos <alexandresantos1@ua.pt>
2019-10-30 20:33:52 +0000

    added car and polygon classes

---
+++ Projeto1/WebGL_example_21.js
@@ -0,0 +1,389 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  WebGL_example_20.js 
+//
+//  Animating models with global and local transformations.
+//
+//  References: www.learningwebgl.com + E. Angel examples
+//
+//  J. Madeira - October 2015
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+//----------------------------------------------------------------------------
+//
+// Global Variables
+//
+
+var car = new Car();
+
+var count_click =0;
+
+var gl = null; // WebGL context
+
+var shaderProgram = null;
+
+var triangleVertexPositionBuffer = null;
+	
+var triangleVertexColorBuffer = null;
+
+// The GLOBAL transformation parameters
+
+var globalAngleYY = 0.0;
+
+var globalTz = 0.0;
+
+// The local transformation parameters
+
+// The translation vector
+
+var tx = 0.0;
+
+var ty = 0.0;
+
+var tz = 0.0;
+
+// The rotation angles in degrees
+
+var angleXX = 0.0;
+
+var angleYY = 0.0;
+
+var angleZZ = 0.0;
+
+// The scaling factors
+
+var sx = 0.75;
+
+var sy = 0.75;
+
+var sz = 0.75;
+
+// NEW - GLOBAL Animation controls
+
+var globalRotationYY_ON = 1;
+
+var globalRotationYY_DIR = 1;
+
+var globalRotationYY_SPEED = 1;
+
+// NEW - Local Animation controls
+
+var rotationXX_ON = 0;
+
+var rotationXX_DIR = 0;
+
+var rotationXX_SPEED = 0;
+ 
+var rotationYY_ON = 0;
+
+var rotationYY_DIR = 1;
+
+var rotationYY_SPEED = 1;
+ 
+var rotationZZ_ON = 0;
+
+var rotationZZ_DIR = 1;
+
+var rotationZZ_SPEED = 1;
+ 
+// To allow choosing the way of drawing the model triangles
+
+var primitiveType = null;
+ 
+// To allow choosing the projection type
+
+var projectionType = 0;
+ 
+// For storing the vertices defining the triangles
+
+
+//----------------------------------------------------------------------------
+//
+// The WebGL code
+//
+
+//----------------------------------------------------------------------------
+//
+//  Rendering
+//
+
+// Handling the Vertex and the Color Buffers
+
+function initBuffers() {	
+	
+	// Coordinates
+
+	
+
+	console.log(car.get_vertices());
+		
+	triangleVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(car.get_vertices()), gl.STATIC_DRAW);
+	triangleVertexPositionBuffer.itemSize = 3;
+	triangleVertexPositionBuffer.numItems = car.get_vertices().length / 3;			
+
+	// Associating to the vertex shader
+	
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
+			triangleVertexPositionBuffer.itemSize, 
+			gl.FLOAT, false, 0, 0);
+	
+	// Colors
+		
+	triangleVertexColorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(car.get_colors()), gl.STATIC_DRAW);
+	triangleVertexColorBuffer.itemSize = 3;
+	triangleVertexColorBuffer.numItems = car.get_colors().length / 3;			
+
+	// Associating to the vertex shader
+	
+	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 
+			triangleVertexColorBuffer.itemSize, 
+			gl.FLOAT, false, 0, 0);
+}
+
+//----------------------------------------------------------------------------
+
+//  Drawing the model
+
+function drawModel( angleXX, angleYY, angleZZ, 
+					sx, sy, sz,
+					tx, ty, tz,
+					mvMatrix,
+					primitiveType ) {
+
+    // Pay attention to transformation order !!
+    
+	mvMatrix = mult( mvMatrix, translationMatrix( tx, ty, tz ) );
+						 
+	mvMatrix = mult( mvMatrix, rotationZZMatrix( angleZZ ) );
+	
+	mvMatrix = mult( mvMatrix, rotationYYMatrix( angleYY ) );
+	
+	mvMatrix = mult( mvMatrix, rotationXXMatrix( angleXX ) );
+	
+	mvMatrix = mult( mvMatrix, scalingMatrix( sx, sy, sz ) );
+						 
+	// Passing the Model View Matrix to apply the current transformation
+	
+	var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+	
+	gl.uniformMatrix4fv(mvUniform, false, new Float32Array(flatten(mvMatrix)));
+	
+	// Drawing the contents of the vertex buffer
+	
+	// primitiveType allows drawing as filled triangles / wireframe / vertices
+	
+	if( primitiveType == gl.LINE_LOOP ) {
+		
+		// To simulate wireframe drawing!
+		
+		// No faces are defined! There are no hidden lines!
+		
+		// Taking the vertices 3 by 3 and drawing a LINE_LOOP
+		
+		var i;
+		
+		for( i = 0; i < triangleVertexPositionBuffer.numItems / 3; i++ ) {
+		
+			gl.drawArrays( primitiveType, 3 * i, 3 ); 
+		}
+	}	
+	else {
+				
+		gl.drawArrays(primitiveType, 0, triangleVertexPositionBuffer.numItems); 
+		
+	}	
+}
+
+//----------------------------------------------------------------------------
+
+//  Drawing the 3D scene
+
+function drawScene() {
+	
+	var pMatrix;
+	
+	var mvMatrix = mat4();
+	
+	// Clearing the frame-buffer and the depth-buffer
+	
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	// Computing the Projection Matrix
+	
+
+
+	// A standard view volume.
+	
+	// Viewer is at (0,0,0)
+	
+	// Ensure that the model is "inside" the view volume
+	
+	pMatrix = perspective( 45, 1, 0.05, 15 );
+	
+	// Global transformation !!
+	
+	globalTz = -2.5;
+
+	
+	
+	// Passing the Projection Matrix to apply the current projection
+	
+	var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	
+	gl.uniformMatrix4fv(pUniform, false, new Float32Array(flatten(pMatrix)));
+	
+	// GLOBAL TRANSFORMATION FOR THE WHOLE SCENE
+	
+	mvMatrix = translationMatrix( 0, 0, globalTz );
+	
+	// Instantianting the current model
+
+		
+	drawModel( angleXX, angleYY, angleZZ, 
+	           sx, sy, sz,
+	           tx, ty, tz,
+	           mvMatrix,
+	           primitiveType );
+}
+
+//----------------------------------------------------------------------------
+//
+//  NEW --- Animation
+//
+
+// Animation --- Updating transformation parameters
+
+// Timer
+
+function tick() {
+	
+	
+	drawScene();
+	
+
+}
+
+
+
+function init_car_position(){
+	ty -= 0.5;
+}
+
+
+
+//----------------------------------------------------------------------------
+//
+//  User Interaction
+//
+
+function outputInfos(){
+    
+}
+
+//----------------------------------------------------------------------------
+
+function setEventListeners(){
+	
+    document.addEventListener("keypress", function(event){
+				
+			var bgColor = document.getElementById("bg-color");
+
+			// Getting the pressed key and setting the bg color
+		
+			var key = event.keyCode; // ASCII
+			console.log(key);
+			switch(key){
+				case 97:
+					if (count_click > -2){
+						tx -= 0.5;
+						//angleZZ-=5;
+						count_click--;	
+					} 
+
+				break;
+				case 100:
+					if (count_click < 2){
+						tx += 0.5;
+						//angleZZ+=5;
+						count_click++;	
+					} 
+				break;
+				
+			}
+			drawScene();
+
+		});     
+}
+
+//----------------------------------------------------------------------------
+//
+// WebGL Initialization
+//
+
+function initWebGL( canvas ) {
+	try {
+		
+		// Create the WebGL context
+		
+		// Some browsers still need "experimental-webgl"
+		
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+		
+
+		
+		// DEFAULT: The viewport occupies the whole canvas 
+		
+		// DEFAULT: The viewport background color is WHITE
+		
+		// NEW - Drawing the triangles defining the model
+		
+		primitiveType = gl.TRIANGLES;
+		
+		// DEFAULT: Face culling is DISABLED
+		
+		// Enable FACE CULLING
+		
+		gl.enable( gl.CULL_FACE );
+		
+		// DEFAULT: The BACK FACE is culled!!
+		
+		// The next instruction is not needed...
+		
+		gl.cullFace( gl.BACK );
+        
+	} catch (e) {
+	}
+	if (!gl) {
+		alert("Could not initialise WebGL, sorry! :-(");
+	}        
+}
+
+//----------------------------------------------------------------------------
+
+function runWebGL() {
+	
+	var canvas = document.getElementById("my-canvas");
+	
+	initWebGL( canvas );
+
+	shaderProgram = initShaders( gl );
+	
+	setEventListeners();
+	
+	initBuffers();
+
+	init_car_position();
+	
+	tick();		// NEW --- A timer controls the rendering / animation    
+
+	outputInfos();
+}
+
+

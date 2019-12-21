eaf5b8529b5a686ea6fa66a73e6bf32dd21abd52
Alexandre <alexandresantos1@ua.pt>
2019-12-21 20:28:20 +0000

    added number detection

--- Projeto2-OpenCV/p.cpp
+++ Projeto2-OpenCV/p.cpp
@@ -13,6 +13,7 @@ int main(){
   // Create a VideoCapture object and open the input file
   // If the input is the web camera, pass 0 instead of the video file name
   VideoCapture cap(0); 
+  
     
   // Check if camera opened successfully
   if(!cap.isOpened()){
@@ -41,22 +42,36 @@ int main(){
     namedWindow( "Frame", WINDOW_AUTOSIZE );
 
     
-
+    cvtColor( frame, frame, COLOR_BGR2GRAY );
+    blur( frame, frame, Size(3,3) );
     // Binary Threshold
     threshold(frame,framet, thresh, maxValue, THRESH_BINARY );
     Mat canny_output;
     vector<Vec4i> hierarchy;
     vector<vector<Point> > contours;
-   
+    
     /// Detect edges using canny
     Canny( framet, canny_output, thresh, thresh*2, 3 );
     /// Find contours
     findContours( canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE , Point(0, 0) );
+    for(int i =0 ;i< contours.size();i++){
+      cout << contours[i] << endl;
+    }
     Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+   
+    vector<vector<Point> > contours_poly( contours.size() );
+    vector<Rect> boundRect( contours.size() );
+    
+    for( size_t i = 0; i < contours.size(); i++ )
+    {
+        approxPolyDP( contours[i], contours_poly[i], 3, true );
+        boundRect[i] = boundingRect( contours_poly[i] );
+    }
     for( size_t i = 0; i< contours.size(); i++ )
     {
-        Scalar color = Scalar(0, 255, 0 );
-        drawContours( drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0 );
+        drawContours( drawing, contours, (int)i, Scalar(0, 255, 0 ), 2, LINE_8, hierarchy, 0 );
+        rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), Scalar(255, 0, 0 ), 2 );
+
     }
     imshow( "Contours", drawing );
    
